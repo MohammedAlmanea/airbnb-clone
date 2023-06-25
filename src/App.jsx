7779f5507949bbbb8ce38b9ae91a53d3417f0b5a
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-06-25 16:42:28 +0300

    Added card component to the app with the data from data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
 import { NavBar } from './components/navbar'
 import { Hero } from './components/Hero'
+import Card from './components/Card'
+import data from './data'
 import './App.css'
 
 
 function App() {
+  
+  const dataElement = data.map(data => {
+    return (
+  <Card
+   key={data.id}
+   data={data}
+   /> 
+    )
+})
 
   return (
     <div>
       <NavBar/>
       <Hero/>
+      <div className='cards-list'>
+      {dataElement}
+      </div>
     </div>
   )
 }
