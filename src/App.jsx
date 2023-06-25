e7bd5125031f4c1bd57da193a825526d7527e753
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-06-25 15:26:51 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavBar } from './components/navbar'
+import { Hero } from './components/Hero'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,8 @@
+
+export const NavBar = ()=> {
+    return (
+        <nav>
+            <img src="../../public/airbnb-logo.png" alt="AirBnb logo" />
+        </nav>
+    )
+}
